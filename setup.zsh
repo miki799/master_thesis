7d777d41d2ee9bf9b11aed819b84d37361d58e16
miki799 <mikolaj.telec@gmail.com>
2024-04-29 13:48:49 +0200

    make nginx server use ssl

--- setup.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-#1 Create namespaces
-
-kubectl apply -f namespaces.yaml
-
-#2 Deploy nginx-svc service (NodePort), nginx-cm (ConfigMap) and nginx (Pod)
-
-kubectl apply -f nginx/nginx.yaml
