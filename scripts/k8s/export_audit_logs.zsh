7fafdfffc3ebca383bf946e404c3d3c135224b5d
miki799 <mikolaj.telec@gmail.com>
2024-05-20 22:37:56 +0200

    add cluster-scoped pss and lower audit logs amount

---
+++ scripts/k8s/export_audit_logs.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+source scripts/variables.zsh
+
+NEW_LOGS_DIR=$AUDIT_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S')
+
+mkdir -p $NEW_LOGS_DIR
+
+docker exec $CP_NODE_NAME sh -c 'cat /var/log/kubernetes/kube-apiserver-audit.log' > $NEW_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S').txt
