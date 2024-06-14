f13412331e87f09fa683edfea19fe8c0afb83cf8
miki799 <mikolaj.telec@gmail.com>
2024-06-14 15:28:54 +0200

    add audits logs export in json

--- scripts/k8s/export_audit_logs.zsh
+++ scripts/k8s/export_audit_logs.zsh
@@ -7,3 +7,4 @@ NEW_LOGS_DIR=$AUDIT_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S')
 mkdir -p $NEW_LOGS_DIR
 
 docker exec $CP_NODE_NAME sh -c 'cat /var/log/kubernetes/kube-apiserver-audit.log' > $NEW_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S').txt
+docker exec $CP_NODE_NAME sh -c 'cat /var/log/kubernetes/kube-apiserver-audit.log' > $NEW_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S').json
