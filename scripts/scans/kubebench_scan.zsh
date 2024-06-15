c5a0e519764b26b239ff743f037bb81142fdcf01
miki799 <mikolaj.telec@gmail.com>
2024-06-15 10:28:28 +0200

    create docs dir

--- scripts/scans/kubebench_scan.zsh
+++ scripts/scans/kubebench_scan.zsh
@@ -11,22 +11,3 @@ docker cp $WORKER_NODE_NAME:/kube-bench $LOGS_DIR
 docker exec $CP_NODE_NAME sh -c 'mkdir kube-bench; kube-bench run -v 5 --targets=master,policies > kube-bench/$(date '+%Y-%m-%d_%H-%M-%S')_cp.txt'
 
 docker cp $CP_NODE_NAME:/kube-bench $LOGS_DIR
-
-# # kube-bench with job
-
-# echo "Running kube-bench job..."
-# kubectl apply -f $K8S_SEC_DIR/security-scan/kube-bench.yaml
-
-# echo "Waiting for kube-bench job to complete..."
-# kubectl wait --for=condition=complete --timeout=15s job/kube-bench -n security
-
-# ## look for pod with completed status
-# POD_NAME=$(kubectl get pods -n security --field-selector=status.phase=Succeeded -l app=kube-bench -o jsonpath='{.items[0].metadata.name}')
-
-# if [ -n "$POD_NAME" ]; then
-#   kubectl logs -n security "$POD_NAME" > "$ROOT_DIR/logs/$(date '+%Y-%m-%d_%H-%M-%S')_kube-bench-logs.txt"
-
-#   echo "Logs retrieved from kube-bench pod: kube-bench-logs.txt"
-# else
-#   echo "No completed kube-bench pod found in the security namespace."
-# fi
--- scripts/scans/kubesec_scan.zsh
+++ scripts/scans/kubesec_scan.zsh
@@ -9,7 +9,10 @@ echo "Security risk analysis for K8s resources..."
 NEW_LOGS_DIR=$KUBESEC_LOGS_DIR/$(date '+%Y-%m-%d_%H-%M-%S')
 
 mkdir -p $NEW_LOGS_DIR
-docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/alpine-dev/alpine-dev.yaml > $NEW_LOGS_DIR/alpine-dev.logs
-docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/nginx/nginx.yaml > $NEW_LOGS_DIR/nginx.logs
-docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/vuln-app/vuln-app.yaml > $NEW_LOGS_DIR/vuln-app.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/alpine-dev/alpine-dev.yaml > $NEW_LOGS_DIR/alpine-dev_secured.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/nginx/nginx.yaml > $NEW_LOGS_DIR/nginx_secured.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_SEC_DIR/vuln-app/vuln-app.yaml > $NEW_LOGS_DIR/vuln-app_secured.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_VULN_DIR/alpine-dev/alpine-dev.yaml > $NEW_LOGS_DIR/alpine-dev_basic.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_VULN_DIR//nginx/nginx.yaml > $NEW_LOGS_DIR/nginx_basic.logs
+docker run -i --rm $KUBESEC scan /dev/stdin < $K8S_VULN_DIR//vuln-app/vuln-app.yaml > $NEW_LOGS_DIR/vuln-app_basic.logs
 echo "Analysis finished!"
