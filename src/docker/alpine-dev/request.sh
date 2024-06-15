24eb91a64d41b3562d0e96ebf812ac9c685ede97
miki799 <mikolaj.telec@gmail.com>
2024-06-15 18:18:13 +0200

    rework networkpolicies and expose vuln-app

--- src/docker/alpine-dev/request.sh
+++ src/docker/alpine-dev/request.sh
@@ -4,6 +4,6 @@
 sleep 10
 
 while true; do
-    curl --no-buffer --cacert /etc/secret/tls.crt https://nginx-svc.dev.svc.cluster.local:4430
+    curl --no-buffer --connect-timeout 5 --cacert /etc/secret/tls.crt https://nginx-svc.dev.svc.cluster.local:4430
     sleep 15
 done
