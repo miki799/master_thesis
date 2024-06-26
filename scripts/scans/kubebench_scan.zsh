917cf8891f2378a5553f2bd6f239a70fd52fd93a
miki799 <mikolaj.telec@gmail.com>
2024-06-26 18:04:08 +0200

    minor changes

--- scripts/scans/kubebench_scan.zsh
+++ scripts/scans/kubebench_scan.zsh
@@ -8,6 +8,6 @@ docker exec $WORKER_NODE_NAME sh -c 'mkdir kube-bench; kube-bench run -v 5 --tar
 
 docker cp $WORKER_NODE_NAME:/kube-bench $LOGS_DIR
 
-docker exec $CP_NODE_NAME sh -c 'mkdir kube-bench; kube-bench run -v 5 --targets=master,policies > kube-bench/$(date '+%Y-%m-%d_%H-%M-%S')_cp.txt'
+docker exec $CP_NODE_NAME sh -c 'mkdir kube-bench; kube-bench run -v 5 --targets=master > kube-bench/$(date '+%Y-%m-%d_%H-%M-%S')_cp.txt'
 
 docker cp $CP_NODE_NAME:/kube-bench $LOGS_DIR
