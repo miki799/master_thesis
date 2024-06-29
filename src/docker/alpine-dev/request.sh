23527082fb04f582ece53609149dd518cf1a4f78
miki799 <mikolaj.telec@gmail.com>
2024-06-29 09:15:00 +0200

    minor changes

--- src/docker/alpine-dev/request.sh
+++ src/docker/alpine-dev/request.sh
@@ -4,6 +4,7 @@
 sleep 10
 
 while true; do
-    curl --no-buffer --connect-timeout 5 --cacert /etc/secret/ca.crt https://nginx-svc.dev.svc.cluster.local:4430
+    curl --no-buffer --connect-timeout 5 --cacert /etc/secret/ca.crt \
+    https://nginx-svc.dev.svc.cluster.local:4430
     sleep 15
 done
